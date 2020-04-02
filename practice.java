d6b40e5f0a97e4866f95bf517414be12f94c3286
Aaron <roycet000@gmail.com>
2020-04-01 20:00:06 -0700

    Add files via upload
    Week 1 extra practice

---
+++ practice.java
@@ -0,0 +1,38 @@
+package practice;
+
+public class practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double itemPriceA1 = 755.55;
+		double walletA1 = 1255.85;
+		int friendsA1 = 9;
+		int ageYearsA1 = 39;
+		String firstNameA1 = "Aaron";
+		String lastNameA1 = "Thomas";
+		char middleInitialA1 = 'W';
+		
+		walletA1 = walletA1 - itemPriceA1;
+		friendsA1 = ageYearsA1 / friendsA1;
+		
+		System.out.println ("Subject's Balance after purchase is: " + walletA1);
+		System.out.println ("Subject's Number of Friends made each year is " + friendsA1);
+		System.out.println ("Subject Name is: " + firstNameA1 + " " + middleInitialA1 + " " + lastNameA1);
+		
+		
+		
+		
+		double itemPriceB1 = 1125.55;
+		double walletB1 = 2255.85;
+		int friendsB1 = 12;
+		int ageYearsB1 = 26;
+		String firstNameB1 = "Erin";
+		String lastNameB1 = "Thompson";
+		char middleInitialB1 = 'L';
+		
+		
+		
+	}
+
+}
